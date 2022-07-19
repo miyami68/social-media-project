14ac6815654d75ffd6c637a110cef7dc6701b0fa
miyami68 <2019ume1843@mnit.ac.in>
2022-07-19 23:18:44 +0530

    added soketio and made real time 1

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c6613fcfe17b5fa3"; }
+/******/ 		__webpack_require__.h = function() { return "4ce63f44d60d2972"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
